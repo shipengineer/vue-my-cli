05da762a601ae5420424507ee525acc52ecfaa0d
PoeticCrane <poeticcrane@gmail.com>
2023-03-14 11:26:43 +0300

    add views

--- src/router/router.js
+++ src/router/router.js
@@ -19,6 +19,42 @@ const routes = [
     meta: { layout: "main" },
     component: () => import("../views/Categories.vue"),
   },
+  {
+    path: "/detailRecord",
+    name: "detailRecord",
+    meta: { layout: "detail" },
+    component: () => import("../views/DetailRecord.vue"),
+  },
+  {
+    path: "/history",
+    name: "history",
+    meta: { layout: "history" },
+    component: () => import("../views/History.vue"),
+  },
+  {
+    path: "/planing",
+    name: "planing",
+    meta: { layout: "planing" },
+    component: () => import("../views/Planing.vue"),
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    meta: { layout: "profile" },
+    component: () => import("../views/Profile.vue"),
+  },
+  {
+    path: "/record",
+    name: "record",
+    meta: { layout: "record" },
+    component: () => import("../views/Record.vue"),
+  },
+  {
+    path: "/register",
+    name: "register",
+    meta: { layout: "register" },
+    component: () => import("../views/Register.vue"),
+  },
 ];
 
 const router = createRouter({
