74f144a3b21c09576e783d635df1eedd5e0e8230
PoeticCrane <poeticcrane@gmail.com>
2023-03-14 14:24:06 +0300

    understood the age of course

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{ open: value }">
+    <router-link
+      v-for="link in links"
+      :key="link.target"
+      active-class="active"
+      :to="link.url"
+      :exact="link.exact"
+      ><li>
+        <a href="#" class="waves-effect waves-orange pointer">{{
+          link.title
+        }}</a>
+      </li>
+    </router-link>
+  </ul>
+</template>
+<script>
+export default {
+  props: ["value"],
+  data: () => ({
+    links: [
+      { title: "Счет", url: "/", exact: true },
+      { title: "История", url: "/history" },
+      { title: "Планирование", url: "/planing" },
+      { title: "Новая запись", url: "/record" },
+      { title: "Категории", url: "/categories" },
+    ],
+  }),
+};
+</script>
