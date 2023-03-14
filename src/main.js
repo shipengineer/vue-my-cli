7cdb85c7190bfd827fea1ef562e3ba56e4b2cff2
PoeticCrane <poeticcrane@gmail.com>
2023-03-14 10:27:23 +0300

    INIT

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
-import router from "./router";
+import router from "./router/router.js";
 import store from "./store";
-
+import "materialize-css/dist/js/materialize.min";
 createApp(App).use(store).use(router).mount("#app");
--- src/router/router.js
+++ src/router/router.js
@@ -8,13 +8,16 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/login",
+    name: "login",
+    meta: { layout: "empty" },
+    component: () => import("../views/Login.vue"),
+  },
+  {
+    path: "/categories",
+    name: "categories",
+    meta: { layout: "main" },
+    component: () => import("../views/Categories.vue"),
   },
 ];
 
@@ -22,5 +25,4 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
-
 export default router;
